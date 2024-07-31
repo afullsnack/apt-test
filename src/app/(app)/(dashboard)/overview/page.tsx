5f4dd4e7f544eac4afd46ae143f8bea8d3b196a8
Miracle Friday <miraclef60@gmail.com>
2024-07-31 23:39:32 +0100

    feat: Add `(dashboard)` route group

---
+++ src/app/(app)/(dashboard)/overview/page.tsx
@@ -0,0 +1,13 @@
+import { Main, Section, Container } from '@/app/(app)/components/craft'
+
+export default function Overview() {
+  return (
+    <Main>
+      <Section>
+        <Container>
+          <h1>Overview page</h1>
+        </Container>
+      </Section>
+    </Main>
+  )
+}
