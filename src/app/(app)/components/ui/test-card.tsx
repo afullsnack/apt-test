5275c8384d509e7a4d922ec4b029cc57cca72dee
Miracle Friday <miraclef60@gmail.com>
2024-08-01 22:47:29 +0100

    feat: Create TestCard component

---
+++ src/app/(app)/components/ui/test-card.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { Card, CardTitle, CardFooter, CardContent } from './card'
+import { cn } from 'src/app/(app)/lib/utils'
+
+type Args = {
+  className?: string
+  title: string
+  description: string
+  testCount: number
+  questionsCount: number
+}
+export const TestCard = ({ className, ...props }: Args) => (
+  <Card className={cn(className)}>
+    <CardContent className="p-4 grid gap-2">
+      <CardTitle>{props.title}</CardTitle>
+      <div className="">
+        <span className="text-balance text-sm font-nromal tracking-normal leading-3">
+          {props.description}
+        </span>
+      </div>
+      <CardFooter className="flex w-full items-center justify-between p-0">
+        <span className="font-bold text-sm p-0 text-left">
+          {props.testCount} tests | {props.questionsCount} questions
+        </span>
+        {/* /// Star rating placeholder */}
+      </CardFooter>
+    </CardContent>
+  </Card>
+)
