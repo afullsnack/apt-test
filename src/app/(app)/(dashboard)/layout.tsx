8c9c78696af2ede24e02d40f14b7f514d4b60bf1
Miracle Friday <miraclef60@gmail.com>
2024-08-01 18:42:47 +0100

    fix: Make navigation links highlighted based on the current route

--- src/app/(app)/(dashboard)/layout.tsx
+++ src/app/(app)/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { Inter } from 'next/font/google'
 import { Container, Layout, Section } from '@/app/(app)/components/craft'
@@ -19,6 +21,7 @@ import {
 } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
 import { cn } from '@/app/(app)/lib/utils'
 
 import {
@@ -52,45 +55,66 @@ type Args = {
   children: React.ReactNode
 }
 
-const DashboardLayout = ({ children }: Args) => (
-  <Layout className={cn(inter.className, 'h-screen')}>
-    <body>
-      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-        <div className="hidden border-r bg-muted/40 md:block">
-          <div className="flex h-full max-h-screen flex-col gap-2">
-            <div className="flex h-14 items-center border-b px-4 lg:h-[80px] lg:px-6">
-              <Link href="/" className="flex items-center gap-2 font-semibold">
-                <Image src={logo} style={{ aspectRatio: 1 / 1 }} alt="logo" className="h-6 w-6" />
-                <span className="">Ruco Tech</span>
-              </Link>
-            </div>
-            <div className="flex-1">
-              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                <Link
-                  href="#"
-                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                >
-                  <Home className="h-4 w-4" />
-                  Overview
+const DashboardLayout = ({ children }: Args) => {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  return (
+    <Layout className={cn(inter.className, 'h-screen')}>
+      <body>
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+          <div className="hidden border-r bg-muted/40 md:block">
+            <div className="flex h-full max-h-screen flex-col gap-2">
+              <div className="flex h-14 items-center border-b px-4 lg:h-[80px] lg:px-6">
+                <Link href="/" className="flex items-center gap-2 font-semibold">
+                  <Image src={logo} style={{ aspectRatio: 1 / 1 }} alt="logo" className="h-6 w-6" />
+                  <span className="">Ruco Tech</span>
                 </Link>
-                <Link
-                  href="#"
-                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                >
-                  <Calendar className="h-4 w-4" />
-                  Test Taker
-                  {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+              </div>
+              <div className="flex-1">
+                <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                  <Link
+                    href="/overview"
+                    className={cn(
+                      'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary',
+                      {
+                        'bg-muted': pathname.includes('overview'),
+                      },
+                    )}
+                  >
+                    <Home className="h-4 w-4" />
+                    Overview
+                  </Link>
+                  <Link
+                    href="/test"
+                    className={cn(
+                      'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary',
+                      {
+                        'bg-muted': pathname.includes('test'),
+                      },
+                    )}
+                  >
+                    <Calendar className="h-4 w-4" />
+                    Test Taker
+                    {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                     6
                   </Badge> */}
-                </Link>
-                <Link
-                  href="#"
-                  className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                >
-                  <LogOutIcon className="h-4 w-4" />
-                  Logut{' '}
-                </Link>
-                {/* <Link
+                  </Link>
+                  <Link
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault()
+                      // TODO: call logout function
+                      router.replace('/')
+                    }}
+                    className={cn(
+                      'flex items-center gap-3 rounded-lg px-3 py-2 text-primary transition-all hover:text-primary',
+                    )}
+                  >
+                    <LogOutIcon className="h-4 w-4" />
+                    Logut{' '}
+                  </Link>
+                  {/* <Link
                   href="#"
                   className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                 >
@@ -104,9 +128,9 @@ const DashboardLayout = ({ children }: Args) => (
                   <LineChart className="h-4 w-4" />
                   Analytics
                 </Link> */}
-              </nav>
-            </div>
-            {/* <div className="mt-auto p-4">
+                </nav>
+              </div>
+              {/* <div className="mt-auto p-4">
               <Card x-chunk="dashboard-02-chunk-0">
                 <CardHeader className="p-2 pt-0 md:p-4">
                   <CardTitle>Upgrade to Pro</CardTitle>
@@ -121,53 +145,53 @@ const DashboardLayout = ({ children }: Args) => (
                 </CardContent>
               </Card>
             </div> */}
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col">
-          <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[80px] lg:px-6">
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-                  <Menu className="h-5 w-5" />
-                  <span className="sr-only">Toggle navigation menu</span>
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="left" className="flex flex-col">
-                <nav className="grid gap-2 text-lg font-medium">
-                  <Link href="#" className="flex items-center gap-2 text-lg font-semibold">
-                    <Image
-                      src={logo}
-                      style={{ aspectRatio: 1 / 1 }}
-                      alt="logo"
-                      className="h-6 w-6"
-                    />
-                    <span className="">Ruco Tech</span>
-                  </Link>
-                  <Link
-                    href="#"
-                    className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                  >
-                    <Home className="h-4 w-4" />
-                    Overview
-                  </Link>
-                  <Link
-                    href="#"
-                    className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                  >
-                    <Calendar className="h-4 w-4" />
-                    Test Taker
-                    {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+          <div className="flex flex-col">
+            <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[80px] lg:px-6">
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+                    <Menu className="h-5 w-5" />
+                    <span className="sr-only">Toggle navigation menu</span>
+                  </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="flex flex-col">
+                  <nav className="grid gap-2 text-lg font-medium">
+                    <Link href="#" className="flex items-center gap-2 text-lg font-semibold">
+                      <Image
+                        src={logo}
+                        style={{ aspectRatio: 1 / 1 }}
+                        alt="logo"
+                        className="h-6 w-6"
+                      />
+                      <span className="">Ruco Tech</span>
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                    >
+                      <Home className="h-4 w-4" />
+                      Overview
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                    >
+                      <Calendar className="h-4 w-4" />
+                      Test Taker
+                      {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                       6
                     </Badge> */}
-                  </Link>
-                  <Link
-                    href="#"
-                    className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                  >
-                    <LogOutIcon className="h-4 w-4" />
-                    Logut{' '}
-                  </Link>
-                  {/* <Link
+                    </Link>
+                    <Link
+                      href="#"
+                      className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+                    >
+                      <LogOutIcon className="h-4 w-4" />
+                      Logut{' '}
+                    </Link>
+                    {/* <Link
                     href="#"
                     className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                   >
@@ -181,30 +205,30 @@ const DashboardLayout = ({ children }: Args) => (
                     <LineChart className="h-5 w-5" />
                     Analytics
                   </Link> */}
-                </nav>
-                <div className="mt-auto">
-                  <Card>
-                    <CardHeader>
-                      <CardTitle>Upgrade to Pro</CardTitle>
-                      <CardDescription>
-                        Unlock all features and get unlimited access to our support team.
-                      </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                      <Button size="sm" className="w-full">
-                        Upgrade
-                      </Button>
-                    </CardContent>
-                  </Card>
-                </div>
-              </SheetContent>
-            </Sheet>
-            <div className="w-full flex-1 flex flex-col">
-              <h1 className="text-balance text-lg font-semibold">Hi {'Jake'}, Good Afternoon</h1>
-              <span className="text-balance text-sx font-normal">
-                Lets learn something new today
-              </span>
-              {/* <form>
+                  </nav>
+                  <div className="mt-auto">
+                    <Card>
+                      <CardHeader>
+                        <CardTitle>Upgrade to Pro</CardTitle>
+                        <CardDescription>
+                          Unlock all features and get unlimited access to our support team.
+                        </CardDescription>
+                      </CardHeader>
+                      <CardContent>
+                        <Button size="sm" className="w-full">
+                          Upgrade
+                        </Button>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </SheetContent>
+              </Sheet>
+              <div className="w-full flex-1 flex flex-col">
+                <h1 className="text-balance text-lg font-semibold">Hi {'Jake'}, Good Afternoon</h1>
+                <span className="text-balance text-sx font-normal">
+                  Lets learn something new today
+                </span>
+                {/* <form>
                 <div className="relative">
                   <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                   <Input
@@ -214,38 +238,39 @@ const DashboardLayout = ({ children }: Args) => (
                   />
                 </div>
               </form> */}
-            </div>
-            <div>
-              <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-                <Bell className="h-4 w-4" />
-                <span className="sr-only">Toggle notifications</span>
-              </Button>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="secondary" size="icon" className="rounded-full">
-                    <CircleUser className="h-5 w-5" />
-                    <span className="sr-only">Toggle user menu</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>Settings</DropdownMenuItem>
-                  <DropdownMenuItem>Password</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem className="text-red-600 hover:text-white hover:bg-red-600">
-                    Logout
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </header>
+              </div>
+              <div>
+                <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+                  <Bell className="h-4 w-4" />
+                  <span className="sr-only">Toggle notifications</span>
+                </Button>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="secondary" size="icon" className="rounded-full">
+                      <CircleUser className="h-5 w-5" />
+                      <span className="sr-only">Toggle user menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Settings</DropdownMenuItem>
+                    <DropdownMenuItem>Password</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="text-red-600 hover:text-white hover:bg-red-600">
+                      Logout
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </header>
 
-          {children}
+            {children}
+          </div>
         </div>
-      </div>
-    </body>
-  </Layout>
-)
+      </body>
+    </Layout>
+  )
+}
 
 export default DashboardLayout
