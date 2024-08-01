ddce090a3092ae3e930809c665a20c1a546a790e
Miracle Friday <miraclef60@gmail.com>
2024-08-01 01:25:28 +0100

    refactor: Refacor overview page and dashboard/layout.tsx
    - Added table shadcn-ui component

--- src/app/(app)/(dashboard)/overview/page.tsx
+++ src/app/(app)/(dashboard)/overview/page.tsx
@@ -3,29 +3,179 @@ import { Main, Section, Container } from '@/app/(app)/components/craft'
 import Link from 'next/link'
 
 // import { Badge } from "@/components/ui/badge"
-import { Button } from '@/app/(app)/components/ui/button'
+import { Card, CardTitle, CardHeader, CardContent } from '@/app/(app)/components/ui/card'
+import {
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+} from '@/app/(app)/components/ui/table'
+import { Badge } from '../../components/ui/badge'
 
 export default function Overview() {
   return (
     <Main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <Section>
+      <Section className="!p-0 grid gap-2">
         <div className="flex items-center">
-          <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+          <h1 className="text-lg font-semibold md:text-xl">Overview</h1>
         </div>
-        <div
-          className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-          x-chunk="dashboard-02-chunk-1"
-        >
-          <div className="flex flex-col items-center gap-1 text-center">
-            <h3 className="text-2xl font-bold tracking-tight">You have no products</h3>
-            <p className="text-sm text-muted-foreground">
-              You can start selling as soon as you add a product.
-            </p>
-            <Button className="mt-4">Add Product</Button>
-          </div>
+        <Container className="!p-0 grid grid-cols-4 gap-2 w-full flex-1">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Total test taken</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">56</div>
+              {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Test completed</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">36</div>
+              {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Average test score</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">36</div>
+              {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Last test score</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">09</div>
+              {/* <p className="text-xs text-muted-foreground">+20.1% from last month</p> */}
+            </CardContent>
+          </Card>
+        </Container>
+      </Section>
+      <Section className="!p-0 grid gap-2">
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-xl">Recent activity</h1>
         </div>
-        <Container>
-          <h1>Overview page</h1>
+        <Container className="!p-0 grid w-full flex-1">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Customer</TableHead>
+                <TableHead className="hidden xl:table-column">Type</TableHead>
+                <TableHead className="hidden xl:table-column">Status</TableHead>
+                <TableHead className="hidden xl:table-column">Date</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <div className="font-medium">Liam Johnson</div>
+                  <div className="hidden text-sm text-muted-foreground md:inline">
+                    liam@example.com
+                  </div>
+                </TableCell>
+                <TableCell className="hidden xl:table-column">Sale</TableCell>
+                <TableCell className="hidden xl:table-column">
+                  <Badge className="text-xs" variant="outline">
+                    Approved
+                  </Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                  2023-06-23
+                </TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <div className="font-medium">Olivia Smith</div>
+                  <div className="hidden text-sm text-muted-foreground md:inline">
+                    olivia@example.com
+                  </div>
+                </TableCell>
+                <TableCell className="hidden xl:table-column">Refund</TableCell>
+                <TableCell className="hidden xl:table-column">
+                  <Badge className="text-xs" variant="outline">
+                    Declined
+                  </Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                  2023-06-24
+                </TableCell>
+                <TableCell className="text-right">$150.00</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <div className="font-medium">Noah Williams</div>
+                  <div className="hidden text-sm text-muted-foreground md:inline">
+                    noah@example.com
+                  </div>
+                </TableCell>
+                <TableCell className="hidden xl:table-column">Subscription</TableCell>
+                <TableCell className="hidden xl:table-column">
+                  <Badge className="text-xs" variant="outline">
+                    Approved
+                  </Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                  2023-06-25
+                </TableCell>
+                <TableCell className="text-right">$350.00</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <div className="font-medium">Emma Brown</div>
+                  <div className="hidden text-sm text-muted-foreground md:inline">
+                    emma@example.com
+                  </div>
+                </TableCell>
+                <TableCell className="hidden xl:table-column">Sale</TableCell>
+                <TableCell className="hidden xl:table-column">
+                  <Badge className="text-xs" variant="outline">
+                    Approved
+                  </Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                  2023-06-26
+                </TableCell>
+                <TableCell className="text-right">$450.00</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <div className="font-medium">Liam Johnson</div>
+                  <div className="hidden text-sm text-muted-foreground md:inline">
+                    liam@example.com
+                  </div>
+                </TableCell>
+                <TableCell className="hidden xl:table-column">Sale</TableCell>
+                <TableCell className="hidden xl:table-column">
+                  <Badge className="text-xs" variant="outline">
+                    Approved
+                  </Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                  2023-06-27
+                </TableCell>
+                <TableCell className="text-right">$550.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Container>
+      </Section>
+      <Section className="!p-0 grid gap-2">
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-xl">Take Mock Test</h1>
+        </div>
+        <Container className="!p-0 grid w-full flex-1">
+          <h1>cards</h1>
         </Container>
       </Section>
     </Main>
