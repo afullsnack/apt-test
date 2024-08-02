3d9c1df6456b2f5a403d77f6ce58f567f88adf7d
Miracle Friday <miraclef60@gmail.com>
2024-08-02 04:45:53 +0100

    refactor: Refactor `test-card` component

--- src/app/(app)/components/ui/test-card.tsx
+++ src/app/(app)/components/ui/test-card.tsx
@@ -11,21 +11,21 @@ type Args = {
 }
 export const TestCard = ({ className, ...props }: Args) => (
   <Card className={cn(className, 'border border-border/45')}>
-    <CardHeader className="!p-4">
-      <CardTitle>{props.title}</CardTitle>
+    <CardHeader className="!px-4">
+      <CardTitle className="font-bold text-lg">{props.title}</CardTitle>
     </CardHeader>
-    <CardContent className="p-4 grid gap-2">
+    <CardContent className="px-4 grid gap-2">
       <div className="">
         <span className="text-balance text-sm font-nromal tracking-normal leading-3">
           {props.description}
         </span>
       </div>
-      <CardFooter className="flex w-full items-center justify-between p-0">
-        <span className="font-bold text-sm p-0 text-left">
-          {props.testCount} tests | {props.questionsCount} questions
-        </span>
-        {/* /// Star rating placeholder */}
-      </CardFooter>
     </CardContent>
+    <CardFooter className="flex w-full items-center justify-between px-4">
+      <span className="font-bold text-sm p-0 text-left">
+        {props.testCount} tests | {props.questionsCount} questions
+      </span>
+      {/* /// Star rating placeholder */}
+    </CardFooter>
   </Card>
 )
