78bd10c6f3d786a1fd86ff1f3630d9abc3f2cf18
Miracle Friday <miraclef60@gmail.com>
2024-08-05 21:55:55 +0100

    fix: Fix main test page question options, with `next` and `back` buttons

--- src/app/(app)/(dashboard)/(test)/[...slug]/page.tsx
+++ src/app/(app)/(dashboard)/(test)/[...slug]/page.tsx
@@ -1,6 +1,8 @@
 import { TestEntry } from '@app/components/test-entry'
 import { SectionEntry } from '@app/components/test-section-entry'
 import { Main, Section, Container } from '@app/components/craft'
+import { Question } from '@app/components/test-questions'
+import { Button } from '@/app/(app)/components/ui/button'
 
 export default async function TestPage({ params }: { params: { slug: string[] } }) {
   console.log(params.slug, ':::from page')
@@ -10,9 +12,23 @@ export default async function TestPage({ params }: { params: { slug: string[] } 
 
   return (
     <Main className="flex flex-1 w-full flex-col gap-4">
-      <Section className="grid gap-2 bg-blue-600/75">
-        <div />
-      </Section>
+      {test && section && !question && (
+        <Section className="grid gap-2 bg-blue-600/75">
+          <div />
+        </Section>
+      )}
+      {test && section && question && (
+        <Section className="!p-8 w-full border border-blue-300 flex items-center justify-between">
+          <h1>{section}</h1>
+          <span>00:00</span>
+          <div className="flex items-center space-x-2">
+            <span>Question {question} of 50</span>
+            <Button variant="outline" size={'sm'}>
+              Close
+            </Button>
+          </div>
+        </Section>
+      )}
 
       {/* test page entry */}
       {test && !section && !question && (
@@ -45,7 +61,7 @@ export default async function TestPage({ params }: { params: { slug: string[] } 
           attempts={2}
         />
       )}
-
+      {/* section page entry */}
       {test && section && !question && (
         <SectionEntry
           section={{
@@ -54,6 +70,11 @@ export default async function TestPage({ params }: { params: { slug: string[] } 
             questionCount: 50,
           }}
         />
+      )}
+
+      {/* test page entry */}
+      {test && section && question && (
+        <Question section={section} test={test} question={question} />
       )}
     </Main>
   )
