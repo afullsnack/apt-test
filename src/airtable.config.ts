485d6aef894e3f829109a22a70a7f2092f8a77c2
Miracle Friday <miraclef60@gmail.com>
2024-08-13 17:13:47 +0100

    feat: Build `Airtable` interface to interact with the bare REST API primitives
    - Remove airtable package
    - Test interface with dynamic route

--- src/airtable.config.ts
+++ src/airtable.config.ts
@@ -1,19 +1,106 @@
-import Airtable from 'airtable'
+/*
+ * Interfase requirements:
+ * - Quesry the airtable API resource
+ * - Fetch details on various resource types
+ * - Validate and parse/decode various resource types
+ */
 
-const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY_TEST })
+// TODO: Create Airtable interface
+type AirtableConfig = {
+  token: string
+  baseId?: string
+}
+class Airtable {
+  private token: string
+  private baseId?: string
+  private baseUrl: string = 'https://api.airtable.com/v0/meta'
+  constructor({ token, baseId }: AirtableConfig) {
+    // TODO: return object with config options
+    this.token = token
+    if (baseId && baseId.length) {
+      this.baseId = baseId
+    }
 
-const base = airtable.base('apptppBpE0rStopjr')
+    return this
+  }
 
-export { base, airtable }
+  // Get base schema:
+  //> Tables with table model
+  //> Fields with field model
+  //> Views with view model
+  async base(baseId?: string) {
+    const BASE_ID = baseId ?? this.baseId
+    if (BASE_ID) {
+      const response = await fetch(`${this.baseUrl}/bases/${BASE_ID}/tables`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      })
 
-// const config = {
-//   base: async () => {
-//     await fetch(`https://api.airtable.com/v0/appJcCgdcLSGKwXHP`, {
-//       headers: { Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}` },
-//     })
-//       .then(async (res) => await res.json())
-//       .catch((err) => console.log(err, ':::error querying airtable base'))
-//   },
-// }
+      if (response.ok) {
+        const json = await response.json()
+        console.log(json, ':::getting specific base schema')
+        return json
+      }
+    }
 
-// export default config
+    throw new NoBaseIdError('No base ID was found for this Airtable instance')
+  }
+
+  // Get records for specific table
+  // > path: /{baseId}/{tableIdOrName}
+  // > pageSize: 100 - default
+  // > offset: if more records after {pageSize} response will contain {offset}
+  // > records: [object of row]
+  async listTableRecords(tableId: string) {
+    const BASE_ID = this.baseId
+    if (BASE_ID) {
+      const response = await fetch(`${this.baseUrl}/${BASE_ID}/${tableId}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      })
+
+      if (response.ok) {
+        const json = await response.json()
+        console.log(json, ':::list table records')
+        return json
+      } else {
+        throw new Error('Something went wrong with the request')
+      }
+    }
+
+    throw new NoBaseIdError('No base ID was found for this Airtable instance')
+  }
+
+  async allBases() {
+    // TODO: call base details
+    const response = await fetch(`${this.baseUrl}/bases`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    })
+
+    if (response.ok) {
+      const json = await response.json()
+      console.log(json, ':::json response from fetching bases')
+      return { bases: json['bases'] }
+    }
+  }
+}
+
+export class NoBaseIdError extends Error {
+  private _tag = 'NoBaseIdError'
+  constructor(message: string) {
+    super(message)
+    super.name = this._tag
+    super.message = message
+    console.error(`[${this._tag}]: ${message}`)
+  }
+}
+
+export { Airtable }
+export type { AirtableConfig }
--- src/app/(app)/(dashboard)/(test)/[...slug]/page.tsx
+++ src/app/(app)/(dashboard)/(test)/[...slug]/page.tsx
@@ -6,12 +6,37 @@ import { Button } from '@app/components/ui/button'
 import { FinishTest } from '@app/components/finish'
 import { ConfirmCloseDialog } from '@/app/(app)/components/confirm-close-dialog'
 import CountdownTimer from '@/app/(app)/components/countdown-timer'
+import { Airtable, NoBaseIdError } from '@/airtable.config'
 
 export default async function TestPage({ params }: { params: { slug: string[] } }) {
   console.log(params.slug, ':::from page')
   const [test, section, question, ...props] = params.slug.slice(1)
+  let sections: Array<any>
 
   console.log(test, section, question, ':::test taker')
+  // TODO: call airtable read to fetch all tables from base
+  const a = new Airtable({ token: process.env.AIRTABLE_API_KEY_TEST! })
+  try {
+    const base = await a.base()
+    console.log(base, ':::single base data')
+    sections = base['tables']?.map((table: any) => ({
+      id: table?.id,
+      name: table?.name,
+      questionCount: 30,
+    }))
+  } catch (e: unknown) {
+    if (e instanceof NoBaseIdError) {
+      const base = await a.base('apptppBpE0rStopjr')
+      console.log(base, ':::single base data retry')
+      sections = base['tables']?.map((table: any) => ({
+        id: table?.id,
+        name: table?.name,
+        questionCount: 30,
+      }))
+    } else {
+      throw e
+    }
+  }
 
   return (
     <Main className="flex flex-1 w-full flex-col gap-4">
@@ -45,28 +70,7 @@ export default async function TestPage({ params }: { params: { slug: string[] } 
         <TestEntry
           title="Mock Test"
           description="Lorem ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsum ipsumipsum"
-          sections={[
-            {
-              id: 'quantitative-reasoning',
-              name: 'Quantitative Reasoning',
-              questionCount: 30,
-            },
-            {
-              id: 'verbal-reasoning',
-              name: 'Verbal Reasoning',
-              questionCount: 30,
-            },
-            {
-              id: 'english',
-              name: 'English',
-              questionCount: 30,
-            },
-            {
-              id: 'logic',
-              name: 'Logic',
-              questionCount: 30,
-            },
-          ]}
+          sections={sections}
           totalQuestionCount={120}
           attempts={2}
         />
@@ -74,15 +78,18 @@ export default async function TestPage({ params }: { params: { slug: string[] } 
       {/* section page entry */}
       {test && section && !question && (
         <SectionEntry
-          section={{
-            id: 'quantitative-reasoning',
-            name: 'Quantitative Reasoning',
-            questionCount: 30,
-          }}
+          section={
+            sections.find((value) => value?.id === section) ?? {
+              id: 'quantitative-reasoning',
+              name: 'Quantitative Reasoning',
+              questionCount: 30,
+            }
+          }
         />
       )}
 
       {/* test page entry */}
+      {/* Randomize tests presented to user */}
       {test && section && question && question !== 'finish' && (
         <Question section={section} test={test} question={question} />
       )}
