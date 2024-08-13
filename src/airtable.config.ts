ece65f8fd55c86b10260b5909f9a9a0115e60239
Miracle Friday <miraclef60@gmail.com>
2024-08-13 11:48:08 +0100

    refactor: Update `airtable.config.ts` configuration with the airtable package

--- src/airtable.config.ts
+++ src/airtable.config.ts
@@ -1,11 +1,19 @@
-const config = {
-  base: async () => {
-    await fetch(`https://api.airtable.com/v0/appJcCgdcLSGKwXHP`, {
-      headers: { Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}` },
-    })
-      .then(async (res) => await res.json())
-      .catch((err) => console.log(err, ':::error querying airtable base'))
-  },
-}
+import Airtable from 'airtable'
 
-export default config
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY_TEST })
+
+const base = airtable.base('apptppBpE0rStopjr')
+
+export { base, airtable }
+
+// const config = {
+//   base: async () => {
+//     await fetch(`https://api.airtable.com/v0/appJcCgdcLSGKwXHP`, {
+//       headers: { Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}` },
+//     })
+//       .then(async (res) => await res.json())
+//       .catch((err) => console.log(err, ':::error querying airtable base'))
+//   },
+// }
+
+// export default config
