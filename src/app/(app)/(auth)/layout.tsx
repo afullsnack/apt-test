77c7170e7d5c4423b79708a352e5521284552fe2
Miracle Friday <miraclef60@gmail.com>
2024-08-20 10:07:10 +0100

    feat: Add welcome page and refactor auth layout and `tailwind.config.ts`

--- src/app/(app)/(auth)/layout.tsx
+++ src/app/(app)/(auth)/layout.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+'use client'
 
+import React from 'react'
+import { usePathname } from 'next/navigation'
 import { Container, Layout, Section } from '@/app/(app)/components/craft'
 
 import { Button } from '@/app/(app)/components/ui/button'
@@ -10,20 +12,29 @@ type Args = {
   children: React.ReactNode
 }
 
-const RootLayout = ({ children }: Args) => (
-  <>
-    {children}
-    <footer className="h-12 border-t grid place-items-center border-border dark:border-border fixed bottom-0 right-0 left-0">
-      <Container className="flex flex-col gap-4 md:flex-row w-full justify-center items-center m-0 !p-0">
-        <Link href="/help-center">Help Center</Link>
-        <Link href="/terms-of-service">Terms of Service</Link>
-        <Link href="/privacy-policy">Privacy Policy</Link>
-        <p className="text-muted-foreground">
-          ©2024 <Link href="https://ruco.tech">Ruco tech</Link>
-        </p>
-      </Container>
-    </footer>
-  </>
-)
+const RootLayout = ({ children }: Args) => {
+  const pathname = usePathname()
+  console.log(pathname, ':::pathname')
+
+  if (pathname === '/') {
+    return <>{children}</>
+  }
+
+  return (
+    <>
+      {children}
+      <footer className="h-12 border-t grid place-items-center border-border dark:border-border fixed bottom-0 right-0 left-0">
+        <Container className="flex flex-col gap-4 md:flex-row w-full justify-center items-center m-0 !p-0">
+          <Link href="/help-center">Help Center</Link>
+          <Link href="/terms-of-service">Terms of Service</Link>
+          <Link href="/privacy-policy">Privacy Policy</Link>
+          <p className="text-muted-foreground">
+            ©2024 <Link href="https://ruco.tech">Ruco tech</Link>
+          </p>
+        </Container>
+      </footer>
+    </>
+  )
+}
 
 export default RootLayout
--- src/app/(app)/(auth)/page.tsx
+++ src/app/(app)/(auth)/page.tsx
@@ -3,30 +3,44 @@
 import React from 'react'
 import { Container, Main, Section } from '@/app/(app)/components/craft'
 import { Button } from '@/app/(app)/components/ui/button'
-import { Home } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Label } from '@/app/(app)/components/ui/label'
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from '@/app/(app)/components/ui/card'
-import { Input } from '@/app/(app)/components/ui/input'
-import { Separator } from '@/app/(app)/components/ui/separator'
 import logo from '../assets/logo.png'
+import welcomeHero from '../assets/welcome-img.png'
+import { ArrowRight } from 'lucide-react'
 
 const Index = () => {
   return (
-    <Main className="min-h-screen">
-      <Section className="grid gap-8 grid-cols-1">
-        <Container className="grid place-items-center items-center justify-center">
-          <Image src={logo} alt="logo" className="size-20 my-5" style={{ aspectRatio: 1 / 1 }} />
+    <Main className="h-screen !p-0 overflow-clip">
+      <Section className="grid grid-cols-2 !p-0 h-full w-full">
+        <Container className="flex flex-col gap-4 w-full h-full place-items-start justify-start">
+          <Image
+            src={logo}
+            alt="logo"
+            className="size-20 mb-5 mt-[20%]"
+            style={{ aspectRatio: 1 / 1 }}
+          />
+          <h1 className="font-bold text-3xl">
+            Welcome To <span className="text-[#1E8DCC]">RucoTech</span>
+          </h1>
+          <span className="font-medium text-xl">CBT Training Platform</span>
+          <span className="font-light text-lg">
+            With an intuitive interface and a comprehensive suite of tools, the platform offers
+            dynamic, interactive learning experiences tailored to meet diverse educational needs.
+          </span>
           <Link href="/code" passHref>
-            <Button>Login with access code</Button>
+            <Button>
+              Get started <ArrowRight className="size-4 ml-4" />{' '}
+            </Button>
           </Link>
+        </Container>
+        <Container className="overflow-hidden !p-0 w-full h-full">
+          <Image
+            src={welcomeHero}
+            alt="welcomeHero"
+            // className="h-full"
+            style={{ backgroundSize: 'cover' }}
+          />
         </Container>
       </Section>
     </Main>
